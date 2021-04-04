b4ea868e84a26a274b193594cea2f4d0c14346a9
Anderson de Sa <asilvadesa@gmail.com>
2021-04-04 12:13:16 -0400

    Describe / It

---
+++ cypress/integration/cod3r/describe.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Shold group test...', () => {
+
+    describe('Shold group more specific tests...', () =>{
+        it('A specific test...', () => {
+
+        })
+    })
+
+    it('A external test...', () => {
+
+    })
+    
+})
