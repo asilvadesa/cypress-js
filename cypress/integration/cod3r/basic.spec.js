79cb34030888f0b79b9605a1ff572fabbb65b50e
Anderson de Sa <asilvadesa@gmail.com>
2021-04-04 23:41:53 -0400

    Acessar uma pagina

---
+++ cypress/integration/cod3r/basic.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+// http://wcaquino.me/cypress/componentes.html
+
+describe('Cypress basic', () => {
+    it('Should visit a page and assert title', () => {
+        cy.visit('http://wcaquino.me/cypress/componentes.html')
+        cy.title().should('be.equal', 'Campo de Treinamento')
+        cy.title().should('contain', 'Campo')
+
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento')
+            .and('contain', 'Campo')
+
+        //TODO imprimir o log no console
+        //TODO escrever o title em um campo de texto
+
+    })
+})
